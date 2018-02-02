5cafc3a2b3d12947ddb6dce0e56b6b2b0e3fed9d
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-02-02 16:52:01 +0530

    positive.c

---
+++ POSITIVE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the positive number");
+scanf("%d%d"&+a);
+printf("Enter the negative number");
+scanf("%d%d"&-b);
+printf("the number is positive or negative");
+{
+for(i=0;i<=6;i++)
+}
+getch()
+}
